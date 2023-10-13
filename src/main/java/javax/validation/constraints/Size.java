81d093aa31a4ae79f7be35e2815102eacc03f660
ducanh1910 <anhnguyenduc1910@gmail.com>
2023-10-13 12:18:14 +0700

    spring

---
+++ src/main/java/javax/validation/constraints/Size.java
@@ -0,0 +1,46 @@
+package javax.validation.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = { })
+public @interface Size {
+
+    String message() default "{javax.validation.constraints.Size.message}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    /**
+     * @return size the element must be higher or equal to
+     */
+    int min() default 0;
+
+    /**
+     * @return size the element must be lower or equal to
+     */
+    int max() default Integer.MAX_VALUE;
+
+    /**
+     * Defines several {@link Size} annotations on the same element.
+     *
+     * @see Size
+     */
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        Size[] value();
+    }
+}
+
